3be4cf6ea725a8030790214f8cbabdff2fb19a36
Paschalis Rompanos <prompanos@gwi.com>
2023-01-21 18:51:41 +0200

    add multiple test cases for countbits

--- scala/src/test/scala/CountBitsSuite.scala
+++ scala/src/test/scala/CountBitsSuite.scala
@@ -1,16 +1,44 @@
 import AllChallenges.CountBits.*
 
 import java.math.*
+import scala.util.Random
+
 
 // For more information on writing tests, see
 // https://scalameta.org/munit/docs/getting-started.html
 class CountBitsSuite extends munit.FunSuite {
 
+  test("test cases for count bits") {
 
-  test("example test that succeeds") {
-    val obtained = countBits(10)
-    val expected = 2
-    assertEquals(obtained, expected)
+    val testCases = List(
+      (0, 0),
+      (4, 1),
+      (7, 3),
+      (9, 2),
+      (10, 2),
+      (26, 3),
+      (77231418, 14),
+      (12525589, 11),
+      (3811, 8),
+      (392902058, 17),
+      (1044, 3),
+      (10030245, 10),
+      (183337941, 16),
+      (20478766, 14),
+      (103021, 9),
+      (287, 6),
+      (115370965, 15),
+      (31, 5),
+      (417862, 7),
+      (626031, 12),
+      (89, 4),
+      (674259, 10)
+    )
+
+    testCases.foreach {
+      case (input, expected) =>
+          val obtained = countBits(input)
+          assertEquals(obtained, expected)
+        }
+    }
   }
-
-}
