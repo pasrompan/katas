0a4253991b3587435f0a806073427e1e39a95a51
Paschalis Rompanos <prompanos@gwi.com>
2023-01-21 18:55:55 +0200

    add one more solution

--- scala/src/main/scala/AllChallenges/CountBits.scala
+++ scala/src/main/scala/AllChallenges/CountBits.scala
@@ -4,11 +4,24 @@ import scala.annotation.tailrec
 
 object CountBits {
 
-  def countBits(num: Int): Int =
+  def countBits2(num: Int): Int = {
     @tailrec
-    def countBits(num: Int, acc: Int): Int =
+    def countBits2(num: Int, acc: Int): Int =
       if num == 0 then acc else
-          countBits(num>>1, acc+(num & 1))
-    countBits(num, 0)
+        countBits2(num >> 1, acc + (num & 1))
+
+    countBits2(num, 0)
+  }
+
+
+    def countBits(n: Int): Int = {
+      @tailrec
+      def loop(count: Int, num: Int): Int = num match {
+        case 0 => count
+        case _ => loop(count + (num & 1), num >>> 1)
+      }
+
+      loop(0, n)
+    }
 
 }
